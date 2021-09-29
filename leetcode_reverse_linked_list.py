89f700b75a9046b86b975e630abf60c20f15a592
nagyung <skql771@gmail.com>
2021-09-30 03:13:46 +0900

    'leetcode'

---
+++ leetcode_reverse_linked_list.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node, prev = head, None
+        while node:
+            prev, prev.next, node = node, prev, node.next
+        
+        return prev
+            
+        
