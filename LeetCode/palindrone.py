b9f96aee4b49053c4c11fb6e876cf83d4e93f380
nagggyung <skql771@gmail.com>
2022-03-26 02:41:03 +0900

    commit

--- LeetCode/palindrone.py
+++ LeetCode/palindrone.py
@@ -1,16 +1,14 @@
-from collections import deque
-
-
 class Solution:
     def isPalindrome(self, s: str) -> bool:
 
-        # deque 선언
-        deq = deque()
+        # 빈 문자열 선언
+        res_str = ""
 
-        # 한 문자씩 소문자로 변환하여 큐에 넣기
+        # 한 문자씩 소문자로 변환하여 문자열에 넣기
         for c in s:
             if c.isalnum():
-                deq.append(c.lower())
+                res_str += c.lower()
 
-        reversed_deq = reversed(deq)
-        return list(deq) == list(reversed_deq)
+        reversed_str = res_str[::-1]
+        return res_str == reversed_str
+    
